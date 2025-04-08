ff0ef15510636b719b0f1b424501d4cd880ae90e
Gajaa-Azhura <shinr433@gmail.com>
2025-04-08 19:08:44 +0530

    added corosel

--- script.js
+++ script.js
@@ -46,4 +46,27 @@
             alert("Data cleared.");
         });
     });
-    
+    const swiper = new Swiper('.mySwiper', {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+            640: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            768: {
+                slidesPerView: 3,
+                spaceBetween: 30,
+            },
+        }
+    });
+
